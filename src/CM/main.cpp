f51091d39feaccbf453ccd05517eec14081f516c
SeveralCamper <mikha.alkhimovich@mail.ru>
2022-02-09 15:27:30 +0700

    main: rework 1-st lab

--- src/CM/main.cpp
+++ src/CM/main.cpp
@@ -4,13 +4,13 @@
 #include <iostream>
 #include <fstream>
 
-void print_matrix(float (*matrix)[4], int equations_num, int variables_num) {
+void print_matrix(float **matrix, int equations_num, int variables_num) {
     for (int i = 0; i < equations_num; i++) {
         for (int j = 0; j < variables_num; j++) {
             if (j == variables_num - 1) {
-                std::cout << "|" << std::setw(3) << matrix[i][j];
+                std::cout << "|" << std::setw(4) << matrix[i][j];
             } else {
-                std::cout << std::setw(3) << matrix[i][j] << std::setw(3);
+                std::cout << std::setw(4) << matrix[i][j] << std::setw(3);
             }
         }
         std::cout << std::endl;
@@ -19,67 +19,70 @@ void print_matrix(float (*matrix)[4], int equations_num, int variables_num) {
 }
 
 int main() {
-    int equations_num = 3, variables_num = 4;
-    float matrix[3][4] = {2, 1, -1, 1, -3, 1, 1, -2, 2, -3, 2, 4};
- 
-    std::string path = "src/CM/conf_file.txt";
+    int equations_num, variables_num, alloc_err = 0;
+    float **matrix;
 
-    std::ifstream fin;
-    fin.open(path);
+    std::cout << "Please enter the number of lines" << std::endl;
+    std::cin >> equations_num;
+    std::cout << "Please enter the number of cloumns" << std::endl;
+    std::cin >> variables_num;
 
-    /* if (!fin.is_open()) {
-        std::cout << "File open error" << std::endl;
-    } else {
-        std::cout << "file opened successfully" << std::endl;
-        char ch;
-        int i, j;
-        while(fin.get(ch)) {
-            matrix_dumb[i] = ch;
-            i++;
-        }
+    if ((matrix = (float**)malloc(equations_num * sizeof(float*))) != NULL) {
+        for (int i = 0; i < equations_num; i++) {
+            if ((matrix[i] = (float*)malloc(variables_num * sizeof(float))) != NULL) {
 
-        for (int i = 0; i < 10; i++) {
-            if (matrix_dumb[i] != '\n') {
-                std::cout << matrix_dumb[i] << std::endl;
+            } else {
+                std::cout << "Memory allocation error!" << std::endl;
+                alloc_err = 1;
+                break;
             }
         }
+    } else {
+        std::cout << "Memory allocation error!" << std::endl;
     }
 
     for (int i = 0; i < equations_num; i++) {
         for (int j = 0; j < variables_num; j++) {
-            matrix[i][j] = matrix_dumb[i];
-            if (matrix_dumb[i] == '-') {
-
-            }
-        }
-    } */
-
-    print_matrix(matrix, equations_num, variables_num);
-
-    for (int i = 0; i < equations_num; i++) {
-            float divide = matrix[i][i];
-            for (int j = i; j < variables_num; j++)
-            {
-            matrix[i][j] = matrix[i][j]/divide;
-            }
-
-            for (int l = 0; l < equations_num; l++)
-            {
-            if (l != i){
-                float subtract = matrix[l][i];
-                matrix[l][i] = 0;
-                for (int m = i+1; m < variables_num; m++)
-                {
-                matrix[l][m] =  matrix[l][m] - subtract*matrix[i][m];
-                }
-            }
+            matrix[i][j] = 0;
         }
     }
 
     print_matrix(matrix, equations_num, variables_num);
-  
 
-    fin.close();
+    if (alloc_err != 1) {
+        std::cout << "The memory has been allocated successfully!" << std::endl;
 
+        std::ifstream file("src/CM/conf_file.txt");
+
+        for (int i = 0; i < equations_num; i++){
+            for (int j = 0; j< variables_num; j++) {
+                file >> matrix[i][j];
+            }
+        }
+
+        print_matrix(matrix, equations_num, variables_num);
+
+        for (int i = 0; i < equations_num; i++) {
+                float divide = matrix[i][i];
+                for (int j = i; j < variables_num; j++)
+                {
+                matrix[i][j] = matrix[i][j]/divide;
+                }
+
+                for (int l = 0; l < equations_num; l++)
+                {
+                if (l != i){
+                    float subtract = matrix[l][i];
+                    matrix[l][i] = 0;
+                    for (int m = i+1; m < variables_num; m++)
+                    {
+                    matrix[l][m] =  matrix[l][m] - subtract*matrix[i][m];
+                    }
+                }
+            }
+        }
+
+        print_matrix(matrix, equations_num, variables_num);
+    }
     return 0;
 }
